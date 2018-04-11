404256f9772fe2ab3dda32567abba2164a2464ed
Devin Parkinson <dparky13@icloud.com>
2018-04-11 13:35:23 -0600

    adding agents

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,6 +9,8 @@ import ProtectedRoute from './ProtectedRoute';
 import AuthRoute from './AuthRoute';
 import FetchUser from './FetchUser';
 import Available from './Available';
+import Cities from './Cities';
+import FindHome from './FindHome';
 import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
@@ -21,6 +23,8 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/available' component={Available} />
+            <Route exact path='/cities' component={Cities} />
+            <Route exact path='/find_home' component={FindHome} />
             <AuthRoute exact path='/login' component={Login} />
             <AuthRoute exact path='/register' component={Register} />
             <Route component={NoMatch} />
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   namespace :api do
     get 'properties', to: 'properties#index'
+    get 'cities/:city', to: 'properties#city'
+    resources :agents, only: [:index, :show]
   end
 
   #Do not place any routes below this one
